225c4ef35513fbfae857ddf7023bc5a5d110c250
Servio <servio19@gmail.com>
2016-03-26 21:02:14 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var log,ger = require('morgan');
+var bodyParser = require('body-parser');
+var PORT = process.env.PORT || 3000;
+
+
+app.use(logger('dev'));
+app.use(express.static("public"));
+
+
+app.get('*', function(req, res){
+  res.sendFile(process.cwd() + '/public/views/index.html');
+});
+
+
+app.listen(PORT, function(req, res){
+  console.log('Listening on PORT:' + PORT);
+});
