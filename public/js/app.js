4fe8bb5c114d896a20bcd87ff5490dfc866185d3
Servio <servio19@gmail.com>
2016-03-30 12:28:44 -0400

    minor changes to ui-router in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,23 @@
-angular.module('portfolio', ['ui.router'])
-.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
-  $urlRouterProvider.otherwise('/');
+angular
+  .module('portfolio', ['ui.router'])
+  .config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider){
+    $urlRouterProvider.otherwise('/');
 
-  $stateProvider
-  .state('home', {
-    url: '/',
-    templateUrl: '/views/home.html'
-  });
+    $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: '/views/home.html'
+    });
 
-  $stateProvider
-  .state('about', {
-    url: '/about',
-    templateUrl: '/views/about.html'
-  });
+    $stateProvider
+    .state('about', {
+      url: '/about',
+      templateUrl: '/views/about.html'
+    });
 
-  $stateProvider
-  .state('projects', {
-    url: '/projects',
-    templateUrl: '/views/projects.html'
-  });
-
-
-  $locationProvider.html5Mode(true);
-
-});
+    $stateProvider
+    .state('projects', {
+      url: '/projects',
+      templateUrl: '/views/projects.html'
+    });
+  }])
