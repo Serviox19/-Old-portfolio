38637b44807e86e3219bb320d28195c0ea9421b3
Servio Mora <servio.mora19@gmail.com>
2016-04-22 16:48:18 -0400

    sticking to this layout for now

--- public/js/foundation.js
+++ public/js/foundation.js
@@ -1,8 +1,3 @@
 $(document).ready(function(){
   $(document).foundation();
-
-  var elem = new Foundation.OffCanvas(element, options);
-
-  $('#element').foundation('toggle', event, trigger);
-
 });
