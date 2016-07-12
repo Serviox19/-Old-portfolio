fd6e8c3dc0a174d77460a2dc8a5a79738d3502c8
Servio Mora <servio.mora19@gmail.com>
2016-07-12 15:41:36 -0400

    added navbar and removed hashtags from url on click

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
 
-  	$('a[href^="#"]').on('click',function (e) {
-  	    e.preventDefault();
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
 
-  	    var target = this.hash;
-  	    var $target = $(target);
-        
-  	    $('html, body').stop().animate({
-  	        'scrollTop': $target.offset().top
-  	    }, 900, 'swing', function () {
-  	        window.location.hash = target;
-  	    });
-  	});
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
 
   $(window).scroll(function () {
       // console.log($(window).scrollTop());
@@ -22,5 +22,9 @@ $(document).ready(function() {
     if ($(window).scrollTop() < 730) {
       $('.navbar').removeClass('navbar-fixed');
     }
+  });
+
+  $(window).on('hashchange', function(e){
+    history.replaceState ("", document.title, e.originalEvent.oldURL);
   });
 });
